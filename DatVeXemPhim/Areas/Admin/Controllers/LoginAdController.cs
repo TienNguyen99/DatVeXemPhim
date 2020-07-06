23ec3f5368685c69935c62c9a3592cc46bb3995e
TienNguyen99 <cuongem1008@gmail.com>
2020-07-06 16:08:28 +0700

    first commit

---
+++ DatVeXemPhim/Areas/Admin/Controllers/LoginAdController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatVeXemPhim.Areas.Admin.Controllers
+{
+    public class LoginAdController : Controller
+    {
+        // GET: Admin/LoginAd
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
